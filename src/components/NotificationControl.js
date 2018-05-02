4e5f0c04724c9a5b32561521b7910c643a77f098
Octavio Coria <octavioxx@gmail.com>
2018-05-02 16:27:36 +0200

    add Notification Status Toggler

--- src/components/NotificationControl.js
+++ src/components/NotificationControl.js
@@ -6,8 +6,62 @@ import { NotificationIcon } from './icons'
 import { NOTIFICATION_STATUS } from '../constants'
 
 
+const notificationStatusMap = {
+  granted: NOTIFICATION_STATUS.ENABLED,
+  denied: NOTIFICATION_STATUS.DENIED,
+  default: NOTIFICATION_STATUS.UNCONFIRMED
+}
+
+
 /** Shows and controls Notification Status */
-export const NotificationControl = () =>
-  <IconButton>
-    <NotificationIcon status={NOTIFICATION_STATUS.UNCONFIRMED} />
-  </IconButton>
+export class NotificationControl extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      status: notificationStatusMap[Notification.permission]
+    }
+
+    this.handleToggleStatus = this.handleToggleStatus.bind(this)
+
+  }
+
+  handleToggleStatus() {
+    const { status } = this.state
+
+    if ((status === NOTIFICATION_STATUS.UNCONFIRMED)
+      || (status === NOTIFICATION_STATUS.DENIED)) {
+      Notification.requestPermission(permission => {
+        this.setState({
+          status: notificationStatusMap[Notification.permission]
+        })
+      })
+
+      return
+    }
+
+    const newStatus = (status === NOTIFICATION_STATUS.ENABLED) ?
+      NOTIFICATION_STATUS.DISABLED :
+      NOTIFICATION_STATUS.ENABLED
+
+    this.setState({
+      status: newStatus
+    })
+  }
+
+
+  render() {
+
+    if (!('Notification' in window)) {
+      return null
+    }
+
+    return (
+      <IconButton onClick={this.handleToggleStatus}>
+        <NotificationIcon status={this.state.status} />
+      </IconButton>
+    )
+  }
+
+}
